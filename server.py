972edbbeca2e96023845bc5cfa99848b2f291cfe
ViirajPatel <77567895+ViirajPatel@users.noreply.github.com>
2022-08-09 12:37:28 +0530

    7

--- server.py
+++ server.py
@@ -13,10 +13,18 @@ def home():
 def upload():
     if(request.method=="POST"):
         imagefile = request.files['image']
+        typeofsoil = request.form.get('soiltype').split(".")[1]
+        print(typeofsoil)
         img = io.imread(imagefile)
         lab = color.rgb2lab(img)
         lightness_avg = average(lab[0])
-        organic_c = 10.44-0.1998*lightness_avg
+        if(typeofsoil=="mid"):
+            organic_c = 10.44-0.1998*lightness_avg
+        elif(typeofsoil=="coarse"):
+            organic_c= 9.16-0.163 * lightness_avg
+        else:
+            organic_c = 7.241 - 0.1342 * lightness_avg
+
         print(organic_c)
         val = {
             'message':str(organic_c)
